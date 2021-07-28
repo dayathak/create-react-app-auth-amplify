d44a920d42e8564e1d607fe6eeeefe44f7183513
Thakur <daythaku@909c4ace63f1.ant.amazon.com>
2021-07-28 10:03:56 -0500

    updated ui components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
+import CreateCase from './CreateCase';
+import ViewCase from './ViewCase';
+import MyCases from './MyCases';
+import Home from './Home';
+
 Amplify.configure(aws_exports);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <AmplifySignOut />
-        <header className="App-header">
+        
+        <Router>
+
+      <div>     
+        <nav>
+          <ul>
+          <li>
+              <NavLink exact to="/" activeClassName="active">CaseManager</NavLink>
+            </li>
+            <li>
+              <NavLink to="/mycases" activeClassName="active">MyCases</NavLink>             
+            </li>
+            <li>
+              <NavLink to="/viewcase" activeClassName="active">ViewCase</NavLink>
+            </li>
+            <li>
+              <NavLink to="/createcase" activeClassName="active">CreateCase</NavLink>
+            </li>                      
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        <Route exact path="/" exact component={Home}/>
+          <Route exact path="/mycases" component={MyCases}/>
+          <Route exact path="/viewcase" component={ViewCase}/>
+          <Route exact path="/createcase" component={CreateCase} />
+        </Switch>
+      </div>
+    </Router>
+    
+ { /*      <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -24,10 +70,13 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+          </header> */}
       </div>
     );
   }
 }
 
-export default withAuthenticator(App);
+
+export default withAuthenticator(App, {
+
+});
---
+++ src/ViewCase.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ViewCase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render(){
+    return (
+       <h1>View Case</h1>
+    );
+  }
+}
